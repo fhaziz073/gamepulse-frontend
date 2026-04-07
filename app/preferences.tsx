28cc02249ff4dad91cbd5b26f06b3b99b423c709
fhaziz073 <faziz2@stevens.edu>
2026-04-07 16:50:45 -0400

    Added network function to preferences page to update user preferences in backend

--- app/preferences.tsx
+++ app/preferences.tsx
@@ -1,13 +1,43 @@
 import { useState } from "react";
 import { FlatList, Image, Pressable, Text, View } from "react-native";
 import EStyleSheet from "react-native-extended-stylesheet";
+import { link } from "./_layout";
+import { useAppSelector } from "./hooks";
 import { ALL_NBA_TEAMS } from "./teams";
 export default function Preferences() {
   const ALL_FAVORITE_TEAMS = ALL_NBA_TEAMS.map((x) => {
     return { ...x, isOn: true };
   });
-  async function changePreferences() {}
+  async function changePreferences(
+    userId: string | undefined,
+    favTeams: typeof ALL_NBA_TEAMS,
+  ) {
+    let response = null;
+    response = await fetch(`${link}/users/pref`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userId,
+        gameStartNotifPref: false,
+        ongoingGameNotifPref: false,
+        favTeams: JSON.stringify({ teams: [] }),
+        favPlayers: JSON.stringify({ players: [] }),
+      }),
+    });
+    console.log(response);
+    if (response !== null && response.status === 201) {
+      console.log("Preferences updated");
+    } else {
+      console.log("Updating preferences failed");
+    }
+  }
   const [images, setImages] = useState(ALL_FAVORITE_TEAMS);
+  const userId = useAppSelector((state) => state.userInfo?.["User ID"]);
+  const selectedTeams = images.filter((x) => {
+    return x.isOn;
+  });
   return (
     <View style={{ flex: 1 }}>
       <FlatList
@@ -35,7 +65,12 @@ export default function Preferences() {
         columnWrapperStyle={{ justifyContent: "space-evenly" }}
         numColumns={3}
       />
-      <Pressable style={styles.submit} onPress={changePreferences}>
+      <Pressable
+        style={styles.submit}
+        onPress={() => {
+          changePreferences(userId, selectedTeams);
+        }}
+      >
         <Text>Submit</Text>
       </Pressable>
     </View>
